999583868ca7bf5371dc9d019fab0cd2a9335e36
Falon Thomas <falont23@gmail.com>
2015-08-02 22:28:03 -0400

    major changes

--- guess.js
+++ guess.js
@@ -1,22 +1,92 @@
+var $humanVsComputerBtn = document.getElementById('game1').getElementsByTagName('button')[0];
 
-var guess = document.querySelector('input[type = number]');
-var btn = document.querySelector('button');
 var randomNumber = Math.floor(Math.random() * 10);
-var response = document.querySelector('h2');
-btn.addEventListener('click', function () {
- var number = parseInt(guess.value);
-  if( number === randomNumber){
-      response.textContent ="WINNER!";
+
+// GAME ONE
+
+$humanVsComputerBtn.addEventListener('click', function () {
+	var $gameElement = this.parentNode;
+	var $userInput = $gameElement.getElementsByTagName('input')[0]
+	var $responseElement = $gameElement.getElementsByTagName('h3')[0]
+	var guess = parseInt( $userInput.value);
+ 	return evaluateGuess(guess, $responseElement);
+});
+
+function evaluateGuess(guess, $responseElement){	 
+	 if( guess=== randomNumber){
+      $responseElement.textContent ="WINNER!";
   }
-  else if( number < randomNumber){
-    response.textContent = "Your Number is too Low!";
+  else if( guess< randomNumber){
+    $responseElement.textContent = "Your guess is too Low!";
   }
-  else if(number >randomNumber){  response.textContent = "Your Number is too High!";
+  else if(guess>randomNumber){  $responseElement.textContent = "Your Number is too High!";
 
   }
   else {
-    response.textContent = "Please enter a number between 1 and 10";
+    $responseElement.textContent = "Please enter a number between 1 and 10";
   }
+ }
+
+// GAME TWO
+
+// var $ComputerVsHumanBtn = document.getElementById('game2').getElementsByTagName('button')[0];
+
+// $ComputerVsHumanBtn.addEventListener('click', function () {
+// 	var $gameElement = this.parentNode;
+// 	var $userInput = $gameElement.getElementsByTagName('input')[0]
+// 	var $responseElement = $gameElement.getElementsByTagName('h3')[0]
+// 	var guess = parseInt( $userInput.value);
+//  	return evaluateGuess(guess, $responseElement);
+// });
+
+// function evaluateGuess(guess, $responseElement){	 
+// 	 if( guess === randomNumber){
+//       $responseElement.textContent ="The Computer Guessed your Number! :)";
+//   }
+//   else if( guess !== randomNumber){
+//     $responseElement.textContent = "The Computer Guessed Wrong! :( ";
+//   }
+
+ // }
 
 
+
+
+// GAME THREE
+
+var $ComputerVsComputer = document.getElementById('game3')        
+  .getElementsByTagName('button')[0];
+
+var result = document.getElementById("game3").getElementsByTagName("h3");
+
+$ComputerVsComputer.addEventListener('click', function () {
+  alert("hi");
+  var upper = 100;
+  var randomDigit = getRandomNumber(upper); 
+  var guess;
+  var attempts = 0;
+
+	function getRandomNumber(upper){
+	  return Math.floor(Math.random()* upper) + 1;
+	}
+	
+	while(guess !== randomDigit){
+	  guess = getRandomNumber(upper);
+	  attempts += 1;
+	}
+  
+  if(guess === randomDigit){
+    
+    div.innerHTML = '<h1>Hello</h1>';
+    
+    // result.appendChild("h3") = "The random number is " + guess + "It took the Computer " + attempts + " tries to guess your number!"
+   alert("help");
+  }
+  
+  
+  
+ 
+
+	// document.getElementById("gam")("The random number is " + guess);
+	// document.write("It took the Computer " + attempts + " tries to guess your number! ");
 });
